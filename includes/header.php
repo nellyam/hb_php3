ca6a125817ad32ca181592b30c12b3d437fbe94b
nellyam <nellynellyam@gmail.com>
2021-01-06 16:51:52 +0100

    ajout du login php

--- includes/header.php
+++ includes/header.php
@@ -11,3 +11,25 @@
   </head>
   <body>
 
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand ml-5 mr-5" href="#">Menu</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link ml-5 mr-5" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link ml-5 mr-5" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link ml-5 mr-5" href="#">Pricing</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled ml-5 mr-5" href="#" tabindex="-1" aria-disabled="true">Location</a>
+      </li>
+    </ul>
+  </div>
+</nav>
---
+++ login.php
@@ -0,0 +1,17 @@
+<head>
+    <!--le lien vers le bootstrap-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title>Les beaux bonnets</title>
+  </head>
+  <body class="bg-danger">
+         <form >
+              <input type="text" name="name" id="name" value="Votre login" class="bg-success p-2 ml-4 mt-5 text-light">
+              <br><br>
+              <input type="text" name="name" id="name" value="Votre mot de passe" class="bg-success p-2 ml-4 text-light">
+              <br><br>
+              <input type="submit" name="name" id="name" value="Valider" class="ml-4 bg-success text-light">
+         </form>
+
+
+  <?php
+  include "includes/footer.php";      
